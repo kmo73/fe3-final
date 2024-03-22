55bf8bda47e5b580496a028b523709ea58b8a919
Javier Pignataro <jgp.workmail@gmail.com>
2024-03-22 20:44:40 -0300

    Cambios para las Routes + Nabvar + Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import { routes } from './Components/utils/routes';
 import Home from './Routes/Home';
-import Contact from './Routes/Contact'
-import Detail from './Routes/Detail'
-import Favs from './Routes/Favs'
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
+import React, { useEffect, useState } from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 
 function App() {
-  return (
-      <>
-        <Routes>
-          <Route path={routes.home} element={<Home/>}/>
-          <Route path={routes.contact} element={<Contact/>}/>
-          <Route path={routes.dentist} element={<Detail/>}/>
-          <Route path={routes.favs} element={<Favs/>}/>
-        </Routes>
-      </>
-  );
+
+      // useEffect(()=> {
+      //       location.replace("/home");
+      // }, [])
+      return (
+            <>
+                  <Navbar/>
+                  <Routes>
+                        <Route exact path={routes.home} element={<Home/>}/>
+                        <Route path={routes.contact} element={<Contact/>}/>
+                        <Route path={routes.dentist} element={<Detail/>}/>
+                        <Route path={routes.favs} element={<Favs/>}/>
+                  </Routes>
+                  <Footer/>
+            </>
+      );
 }
 
 export default App;
