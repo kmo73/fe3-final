14c292236ae43bc8f23217f8802254ab26939577
Camilo Patiño <nelsoncamilopatino@gmail.com>
2024-03-22 20:05:12 -0300

    creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import { Route, Routes } from 'react-router-dom'
+import { routes } from './Components/utils/routes';
+import Home from './Routes/Home';
+import Contact from './Routes/Contact'
+import Detail from './Routes/Detail'
+import Favs from './Routes/Favs'
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+      <>
+        <Routes>
+          <Route path={routes.home} element={<Home/>}/>
+          <Route path={routes.contact} element={<Contact/>}/>
+          <Route path={routes.dentist} element={<Detail/>}/>
+          <Route path={routes.favs} element={<Favs/>}/>
+        </Routes>
+      </>
   );
 }
 
---
+++ src/Components/utils/routes.js
@@ -0,0 +1,6 @@
+export const routes = {
+    home: '/home',
+    contact: '/contact',
+    dentist: '/dentist/:id',
+    favs: '/favs'
+}
